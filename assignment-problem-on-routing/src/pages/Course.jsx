fed917c0b9bc1052436b5618f2df3ccc3226639a
Paras Rajeshlal Gurkhude <parasgurkhude@gmail.com>
2024-05-25 23:21:28 +0530

    All assignments done

---
+++ assignment-problem-on-routing/src/pages/Course.jsx
@@ -0,0 +1,8 @@
+export default function Course() {
+  return (
+    <>
+      <h1>Course</h1>
+    </>
+  )
+}
+
---
+++ assignment-problem-on-routing/src/pages/Fees.jsx
@@ -0,0 +1,9 @@
+export default function Fees(){
+  return (
+    <>
+      <h1>Fees</h1>
+    </>
+  )
+}
+
+
